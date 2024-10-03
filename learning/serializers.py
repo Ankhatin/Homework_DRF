a0980e2828f74c7bc7ea7cd7d76ce9052ff98748
Andrey Khatin <ankhatin@gmail.com>
2024-10-03 20:45:17 +0700

    modifying serializers modules

--- learning/serializers.py
+++ learning/serializers.py
@@ -3,15 +3,22 @@ from rest_framework import serializers
 from learning.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Course
-        fields = "__all__"
-
-
 class LessonSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lessons = LessonSerializer(many=True, read_only=True)
+    lessons_count = serializers.SerializerMethodField()
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
+
+    class Meta:
+        model = Course
+        fields = ['id', 'name', 'description', 'lessons', 'lessons_count']
+
+
