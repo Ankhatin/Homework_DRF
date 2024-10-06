9711c1968400fb94f28e72e88ed8420ef93942f9
Andrey Khatin <ankhatin@gmail.com>
2024-10-06 17:04:46 +0700

    modifying models

--- learning/models.py
+++ learning/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
-from config.settings import MEDIA_ROOT
+# from users.models import User
 
 NULLABLE = {'blank': True, 'null': True}
 
 
-# Create your models here.
 class Course(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
     preview = models.ImageField(upload_to='media/learning', **NULLABLE, verbose_name='Изображение')
+    owner = models.ForeignKey('users.User', on_delete=models.SET_NULL, related_name="user_courses", verbose_name="Пользователь", **NULLABLE)
 
     def __str__(self):
         return f'{self.name}'
@@ -26,6 +26,7 @@ class Lesson(models.Model):
     preview = models.ImageField(upload_to='media/learning', **NULLABLE, verbose_name='Изображение')
     video = models.URLField(max_length=200, **NULLABLE, verbose_name='Ссылка на видео')
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons', verbose_name='Курс')
+    owner = models.ForeignKey('users.User', on_delete=models.SET_NULL, related_name="user_lessons", verbose_name="Пользователь", **NULLABLE)
 
     def __str__(self):
         return f'{self.name}'
