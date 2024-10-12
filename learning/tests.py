09011290c644f319844059d1b44a68949fa5025c
Andrey Khatin <ankhatin@gmail.com>
2024-10-12 14:07:06 +0700

    covering by tests

--- learning/tests.py
+++ learning/tests.py
@@ -1,3 +1,58 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, force_authenticate
+from rest_framework import status
+from django.urls import reverse
 
-# Create your tests here.
+from learning.models import Lesson, Course
+from users.models import User
+
+
+class LessonListTestCase(APITestCase):
+
+    def setUp(self):
+        user = User.objects.create(email='test@test', phone='test', city='test')
+        self.course = Course.objects.create(name="Develop",
+                                       description="real")
+        self.lesson = Lesson.objects.create(name="Java",
+                              description="good",
+                              course=self.course,
+                              owner=user
+                              )
+        Lesson.objects.create(name="Python",
+                              description="great",
+                              course=self.course,
+                              owner=user
+                              )
+
+        self.client.force_authenticate(user=user)
+
+    def test_get_list(self):
+        url = reverse('learning:lessons')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_lesson(self):
+        url = reverse('learning:lesson', kwargs={'pk': self.lesson.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'Java')
+
+    def test_create_lesson(self):
+        self.data = {'name': 'Databases', 'description': 'Базы данных', 'course': 1, 'owner': 1}
+        url = reverse('learning:lesson_create')
+        response = self.client.post(url, self.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_update_lesson(self):
+        url = reverse('learning:lesson_update', kwargs={'pk': self.lesson.pk})
+        self.data = {"name": "C++"}
+        response = self.client.patch(url, self.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_lesson(self):
+        url = reverse("learning:lesson_delete", kwargs={'pk': self.lesson.pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        with self.assertRaises(Exception):
+            Lesson.objects.get(pk=self.lesson.pk)
+
+
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, force_authenticate
+from rest_framework import status
+from django.urls import reverse
 
-# Create your tests here.
+from learning.models import Course
+from users.models import User, Subscribe
+
+
+class SubscribeDeleteTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(email='test@test', phone='test', city='test')
+        self.course = Course.objects.create(name="Develop",
+                                       description="real")
+        self.subscribe = Subscribe.objects.create(user=self.user, course=self.course)
+        self.client.force_authenticate(user=self.user)
+
+    def test_create_subscribe(self):
+        existed_sub = Subscribe.objects.get(user_id=self.user.pk, course_id=self.course.pk)
+        existed_sub.delete()
+        self.data = {'user': self.user.pk, 'course': self.course.pk}
+        url = reverse('users:subscribes', kwargs={'user_id': self.user.pk})
+        response = self.client.post(url, self.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(Subscribe.objects.get(user=self.user, course=self.course))
+
+    def test_delete_subscribe(self):
+        url = reverse('users:subscribes', kwargs={'user_id': self.user.pk})
+        self.data = {'user': self.user.pk, 'course': self.course.pk}
+        response = self.client.post(url, self.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        with self.assertRaises(Exception):
+            Subscribe.objects.get(pk=self.subscribe.pk)
